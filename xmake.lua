93aeec2b4b6d54cdbb511d7236fb225c97876406
engsr6982 <pwxg286704@163.com>
2025-12-22 15:37:28 +0800

    build: add miracleforest-repo and ilistenattentively dependency

--- xmake.lua
+++ xmake.lua
@@ -1,8 +1,10 @@
 add_rules("mode.debug", "mode.release")
 
 add_repositories("levimc-repo https://github.com/LiteLDev/xmake-repo.git")
+add_repositories("miracleforest-repo https://github.com/MiracleForest/xmake-repo.git")
 
 add_requires("levilamina 1.7.0", {configs = {target_type = "server"}})
+add_requires("ilistenattentively 0.10.0")
 
 add_requires("levibuildscript")
 
@@ -14,7 +16,7 @@ target("PermCore") -- Change this to your mod name.
     add_rules("@levibuildscript/linkrule")
     add_cxflags( "/EHa", "/utf-8", "/W4", "/w44265", "/w44289", "/w44296", "/w45263", "/w44738", "/w45204")
     add_defines("NOMINMAX", "UNICODE")
-    add_packages("levilamina")
+    add_packages("levilamina", "ilistenattentively")
     set_exceptions("none") -- To avoid conflicts with /EHa.
     set_kind("static")
     set_languages("c++20")
